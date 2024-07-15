03ce28c8293e194a6f9967b4160f9dae7ad4fc10
Smriti <smriti12sci.nvm@gmail.com>
2024-07-15 16:53:29 +0530

    UI fixes

--- src/components/qColorAnalysis.js
+++ src/components/qColorAnalysis.js
@@ -7,6 +7,7 @@ const QColorAnalysis = () => {
   const [answers, setAnswers] = useState([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [analysisResult, setAnalysisResult] = useState(null);
+  const [showSubmit, setShowSubmit] = useState(false);
 
   const handleOptionSelect = (event) => {
     const optionId = event.target.id;
@@ -20,7 +21,13 @@ const QColorAnalysis = () => {
       { questionId: questionsData[currentQuestion].question, optionText }
     ]);
     setCurrentQuestion(currentQuestion + 1);
+    if (currentQuestion === questionsData.length - 1) {
+      setShowSubmit(true);
+    } else {
+      setCurrentQuestion(currentQuestion + 1);
+    }
   };
+
 
   const handleSubmit = async () => {
     try {
@@ -40,12 +47,21 @@ const QColorAnalysis = () => {
   return (
     <div className="flex h-screen overflow-hidden">
       <Sidebar /> 
-      <div className="flex-1 flex flex-col overflow-y-auto ml-10">
-        <div className="p-4">
-          <h1 className="text-3xl font-bold mb-4 text-pink-500">Color Analysis Test</h1>
-          <div>
+      <div className="flex-1 flex-col overflow-y-auto">
+        <div className="p-4 sticky top-0 z-10 bg-white">
+              <div className="flex-1 border border-gray-300 rounded-full p-4 text-center max-w-[13vw] h-[7vh] md:w-25vw relative group mx-auto">
+                <h2 className="text-s-lg font-semi-bold text-white">Color Analysis Test</h2>
+                <style jsx>{`
+                      .group {
+                        background-image: linear-gradient(to bottom, #ed219f 0%, #e94a2f 90%);
+                      }
+                    `}
+                </style>
+              </div>
+            </div>
+          <div className="flex flex-wrap">
             {questionsData.map((question, index) => (
-              <div key={question.id} style={{ display: index <= currentQuestion ? 'block' : 'none', marginBottom: '20px' }}>
+              <div key={question.id} style={{ display: index <= currentQuestion ? 'block' : 'none', marginBottom: '20px' }} className="mb-8 flex items-start">
                 <div className="mb-10">{question.question}</div>
                 <div className="flex flex-wrap">
                   {question.options.map(option => (
@@ -56,36 +72,40 @@ const QColorAnalysis = () => {
                         name={`question${question.id}`} 
                         onChange={handleOptionSelect} 
                         className="mr-2"
+                        required
                       />
+                      <div className="flex flex-col items-center">
                       <img 
                         src={option.imageUrl} 
                         alt={option.text} 
-                        className="mr-2 w-16 h-16 object-cover" 
+                        className="w-16 h-16 object-cover mb-2 rounded-2xl" 
                       />
-                      <label htmlFor={option.id} className="ml-2">{option.text}</label>
+                      <label htmlFor={option.id} className="mt-2 text-center">{option.text}</label>
+                      </div>
                     </div>
                   ))}
                 </div>
               </div>
             ))}
           </div>
-          {currentQuestion === questionsData.length && (
+          {showSubmit && (
+            <div className="flex justify-center">
             <div>
-              <p className="mb-4">All questions answered. Click submit to proceed.</p>
-              <button onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Submit
               </button>
+            </div>
             </div>
           )}
           {analysisResult && (
             <div className="mt-4">
               <h2 className="text-2xl font-bold">Analysis Result</h2>
               <p>{analysisResult}</p> 
+              <a href={`https://www.myntra.com/women?f=Categories%3ADresses%3A%3AColor%3A${analysisResult}`}>myntra.com</a>
             </div>
           )}
         </div>
       </div>
-    </div>
   );
 };
 
